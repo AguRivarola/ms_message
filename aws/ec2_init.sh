183c0df08e33c88fb4b5e571fbaa162af1b25dad
Agus Rivarola <agurivarola@gmail.com>
2024-03-27 01:39:03 -0300

    Edito para buildear

--- aws/ec2_init.sh
+++ aws/ec2_init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo yum update
-sudo yum -y install docker
+sudo yum -y install docker git
 service docker start
 usermod -a -G docker ec2-user
 chkconfig docker on
 pip3 install docker-compose
+git clone https://github.com/AguRivarola/ms_message.git
